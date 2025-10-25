7788af310efe16d8607e5e127b4a4aba38e69421
littlehx <3040983563@qq.com>
2025-10-25 10:46:01 +0800

    修改action并且添加测试文件

---
+++ test/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :__init__.py
+# @Time      :2025/10/25 10:29
+# @Author    :littlehx
+# @Desc      :
---
+++ test/test_player.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :main
+# @Time      :2025/10/25 10:30
+# @Author    :littlehx
+# @Desc      :
+
+def test_pytest():
+    assert True
